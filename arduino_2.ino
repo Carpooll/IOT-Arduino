1f1bd26d3c862b2b432c3e474cb809788ec3183d
BrayanPr <71664912+BrayanPr@users.noreply.github.com>
2021-06-30 18:15:05 -0600

    Update arduino_2.ino

--- arduino_2.ino
+++ arduino_2.ino
@@ -1,7 +1,9 @@
-int c = 0;
 int state = 0;
-bool flag = true;
-String id; 
+int c;
+int total_money;
+bool on_id = true;
+bool on_money = false;
+
 // include the library code:
 #include <LiquidCrystal.h>
 
@@ -11,48 +13,122 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("Write your ID:");
   Serial.begin(9600);
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  while(flag){
-	state= Serial.read();
-    
-    if(state!=42 && state!=-1){
+	id();
+  
+}
+
+void id(){
+  c=0;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Insert your id:");
+  
+  while(on_id){
+   	state= Serial.read();
+    if(state!=-1){
+      if(state!=42){
       lcd.setCursor(c, 1);
       c++;
 
 
       if(state == 48){
         Serial.println(0);
-      	Serial.println(c);
+        Serial.println(c);
 
         lcd.print(0);
       }else{
         Serial.println(state);
-      	Serial.println(c);
-      	lcd.print(state); 
+        Serial.println(c);
+        lcd.print(state); 
       }
-      
-	  
-    }else if(state==42){
+
+    }else if(state == 42 && c>=0){
       c--;
-      Serial.println(state);
+      Serial.write('borrar');
       Serial.println(c);
-      
-	  lcd.setCursor(c, 1);
+
+      lcd.setCursor(c, 1);
       lcd.print(" ");
-      
+
     }
     if(c==10){
-    	flag = false;  
+      on_id = false;  
+      choice();
     }
   }
-  	lcd.setCursor(0, 0);
-  	lcd.print("It is correct? 1=YES 2=NO");
-    delay(200);  
+  }
+}
+
+void money(){
+  c=0;
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Insert money: ");
+  while(on_money){
+    state= Serial.read();
+    
+    if(state!=-1){
+      if(state!=42){
+      	lcd.setCursor(c, 1);
+      	c++;
+
+        if(state == 48 && total_money>0){
+          total_money*=10;
+          Serial.println(0);
+          Serial.println(c);
+          lcd.print(0);
+      	}else{
+          if(total_money>0){
+            total_money*=10;
+          }
+          total_money+=state;
+          Serial.println(state);
+          Serial.println(c);
+          lcd.print(state); 
+        }
+
+    }else if(state == 42 && c>=0){
+      c--;
+      Serial.println('Borrar');
+      Serial.println(c);
+		
+      lcd.setCursor(c, 1);
+      lcd.print(" ");
+	  
+    }else if(state==35){ 
+      lcd.setCursor(0, 0);
+      lcd.print("Depositing");
+      delay(2300);
+      lcd.clear();
+      lcd.print("Deposite");
+      lcd.setCursor(0,1); 
+      lcd.print("Successfully");
+      on_id = true;
+      on_money = true;
+      id();
+    }
+  }
+    
+  }
+}
+void choice(){
+  lcd.setCursor(0, 0);
+  lcd.print("Is it correct? ");
+  while(state!= 42 && state!=35){
+    state = Serial.read();
+    if(state == 42){
+      on_id=true;
+      on_money=false;
+      id();
+    }
+    if(state == 35){
+      on_id=false;
+      on_money=true;
+      money();
+  }
+  }
 }
